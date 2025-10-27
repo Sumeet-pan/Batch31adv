931b99f3eb805df85c764bae1f5df44177dfbe86
Sumeet Pandey <Sumeet Pandey@Sumeet.lan>
2025-10-27 20:02:59 +0530

    Committed

---
+++ src/test/java/practices1/SampleTest.java
@@ -0,0 +1,12 @@
+package practices1;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test(groups = "Smoke")
+	public void appleTest()
+	{
+		System.out.println("apple tested");
+	}
+
+}
---
+++ src/test/java/practices2/DemoTest.java
@@ -0,0 +1,12 @@
+package practices2;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test(groups = "regression")
+	public void orangeTest()
+	{
+		System.out.println("orange tested");
+	}
+
+}
